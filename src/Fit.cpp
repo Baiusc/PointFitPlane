dd690d26afca2939091ed7f76d80e3f31e344b0d
bzs_work_pc <baizhongshan@51creation.com>
2023-08-03 11:25:22 +0800

    断点无效不加载pdb也不报错时，优先重新生成

--- src/Fit.cpp
+++ src/Fit.cpp
@@ -1,3 +1,5 @@
+#pragma once
+#include "MyFunc.h"
 #include <pcl/ModelCoefficients.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
@@ -6,23 +8,17 @@
 #include <pcl/segmentation/sac_segmentation.h>
 
 
+using namespace MyFunc;
 
-typedef pcl::PointXYZRGB PointT;
-pcl::PointCloud<PointT>::Ptr cloud_src(new pcl::PointCloud<PointT>); //输入的隧道点云
+
+pcl::PointCloud<PointT>::Ptr cloud_input(new pcl::PointCloud<PointT>); //输入的隧道点云
 
 
 #pragma region 详细步骤方法
-void segmentPointCloud(const std::string& filename) {
-
-    // 创建一个点云对象
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-    // 读取点云文件
-    pcl::PCDReader reader;
-    reader.read(filename, *cloud);
+void segmentPointCloud(const pcl::PointCloud<PointT>::Ptr cloud) {
 
     // 创建一个SACSegmentation对象，并设置模型类型为平面，方法类型为RANSAC
-    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::SACSegmentation<PointT> seg;
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
 
@@ -59,26 +55,9 @@ int main(int argc, char** argv)
     point.z = -5.24;
     std::cout << "点: ( " << point.x << " , " << point.y << " , " << point.z << " )" << std::endl;
 
-    readPcd("../cloud/Tunnel.pcd", cloud_src); // 输入：隧道点云
-    std::string filename = "table_scene_lms400.pcd";
-    segmentPointCloud(filename);
+    readPcd("../cloud/Tunnel.pcd", cloud_input); // 输入：隧道点云
+    segmentPointCloud(cloud_input);
 
     return 0;
 }
 
-int main_a(int argc, char** argv)
-{
-
-	float leaf_size = 0.02; // 输入：分辨率
-	readPcd("../cloud/Tunnel.pcd", cloud_src); // 输入：隧道点云
-	//Eigen::Vector3f normal(0.0, 0.0, 1.0); 	// 输入：法向
-	Eigen::Vector3f normal(0.35, 0.22, 0.91); // 模拟非z法向 
-	PointT point; 	//输入：一个三维点 
-	point.x = -1.70;
-	point.y = -4.08;
-	point.z = -5.24;
-	std::cout << "点: ( " << point.x << " , " << point.y << " , " << point.z << " )" << std::endl;
-	std::cout << "分辨率: " << leaf_size << "m" << std::endl;
-
-	return 0;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,8 @@ using namespace MyFunc;
 std::mutex UpdateMutex;
 pcl::PointCloud<PointT>::Ptr cloud_src(new pcl::PointCloud<PointT>); //输入的隧道点云
 //pcl::visualization::PCLVisualizer viewer("没有PCLVisualizer单开vtk会报错"); // Warning: Link to vtkInteractionStyle for default style selection
-int main_a(int argc, char** argv)
+int mainee(int argc, char** argv)
 {
-#ifdef _OPENMP
-	std::cout << "OpenMP support is enabled" << std::endl;
-#else
-	std::cout << "OpenMP support is disabled" << std::endl;
-#endif
 	float leaf_size = 0.02; // 输入：分辨率
 	readPcd("../cloud/Tunnel.pcd", cloud_src); // 输入：隧道点云
 	//Eigen::Vector3f normal(0.0, 0.0, 1.0); 	// 输入：法向
